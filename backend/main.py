751ff9907c6cf56a8f6e610cc1c0acd40aed0357
TT <tharani.theertha@gmail.com>
2025-04-16 16:44:05 +0530

    CORSMiddleware setup

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes.items import router as items_router
 from routes.analytics import router as analytics_router
 from routes.quiz import router as quiz_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(items_router, prefix="/items")
 app.include_router(analytics_router)
